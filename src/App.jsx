3d6de34bce2804e8ac4c84c9b668e1fdd6ee99d4
Om Bhesania <48543687+om-bhesania@users.noreply.github.com>
2022-05-19 17:47:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,7 @@ import Portfolio from './components/portfolio/Portfolio'
 
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
-import ReactGA from 'react-ga'
 
-class App extends Component {
-  setGA = () => {
-    ReactGA.initialize('G-JSZ3GTX4RJ');
-    ReactGA.pageview('Init page view');
-  };
-  componentDidMount(){
-    this.setGA();
-  }
 const App = () => {
   return (
     <>
