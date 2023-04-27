7f6e717fec5ef2667c1fe17a8aed9458a515730a
Om Bhesania <48543687+om-bhesania@users.noreply.github.com>
2023-04-27 13:06:08 +0530

    Update Experience.jsx

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -77,13 +77,6 @@ const Experience = () => {
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4></h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
-              <div>
                 <h4>MySQL</h4>
                 <small className='text-light'>Intermediate</small>
               </div>
